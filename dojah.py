24abb17ea901c3860f847e9487a986aa6070a4e2
Olamidun <kolapoolamidun@gmail.com>
2021-02-23 03:27:36 +0100

    implemented create crypto wallet endpoint

--- dojah.py
+++ dojah.py
@@ -23,7 +23,19 @@ class PyDojah:
         return response.json()
 
 
-dojah = PyDojah('your app id', 'your dojah secret key', sandbox=False)
+    def create_crypto_wallet(self, wallet_type):
+        headers = {'Authorization': self.secret_key, 'AppId': self.app_id}
 
-result = dojah.get_balance()
-print(result.get('entity'))
+        endpoint = f"{self.base_endpoint}/api/v1/wallet/create"
+
+        data = {'wallet_type': wallet_type}
+
+        response = requests.post(endpoint, headers=headers, data=data)
+        return response.json()
+        
+
+
+dojah = PyDojah('5fc144c4318b66003e7644c2', 'test_sk_4NJutv5R4FpQFiyC0SPAPcezX', sandbox=True)
+
+result = dojah.create_crypto_wallet('BTC')
+print(result)
